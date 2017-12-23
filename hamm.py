b862b21e68ea89b575ac99a8eef0f78c7327bf4e
Zane Bradley <zanegbradley@gmail.com>
2017-12-22 20:22:26 -0800

    organize files

--- hamm.py
+++ hamm.py
@@ -1,5 +1,11 @@
 #!/bin/env python3
+
 import sys
+
+"""
+Given: Two DNA strings 's' and 't' of equal length.
+Return: The Hamming distance dH(s,t).
+"""
 
 s = sys.stdin.readline()[:-1]
 t = sys.stdin.readline()[:-1]
@@ -9,5 +15,5 @@ assert len(s) == len(t)
 diff = 0
 for i in range(len(s)):
     if s[i] != t[i]:
-        diff+=1
+        diff += 1
 print(diff)
